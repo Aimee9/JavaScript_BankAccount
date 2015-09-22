be5e57d483ab90d6e8888ae84e6ced5b882a21a5
momo ozawa & aimee reiss <student@epicodus.com>
2015-09-22 11:47:28 -0700

    Pass mocha tests for constructor, deposit, and withdraw functions

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,29 @@
-Example of spec
+describe('BankAccount', function() {
+  it("will create a bank account object", function() {
+    var testAccount = new BankAccount("epicodus", 200, "checking");
+    expect(testAccount.username).to.equal("epicodus");
+    expect(testAccount.balance).to.equal(200);
+    expect(testAccount.accountType).to.equal("checking");
+  });
+});
 
-// describe('factorial', function() {
-//   it("will take a number and multiply it by all other numbers less than it", function() {
-//     expect(factorial(5)).to.equal(120);
-//   });
-// });
+describe('deposit', function() {
+  it("will add an amount of money to a user's account", function() {
+    var testAccount = new BankAccount("epicodus", 200, "checking");
+    testAccount.deposit(100);
+    expect(testAccount.balance).to.equal(300);
+  });
+});
+
+describe('withdraw', function() {
+  it("will subtract an amount of money to a user's account", function() {
+    var testAccount = new BankAccount("epicodus", 200, "checking");
+    testAccount.withdraw(100);
+    expect(testAccount.balance).to.equal(100);
+  });
+
+  it("will not subtract an amount of money to a user's account if amount is greater than existing funds", function() {
+    var testAccount = new BankAccount("epicodus", 200, "checking");
+    expect(function() { testAccount.withdraw(300); }).to.throw(new BankAccountException('Not enough funds! Please try again.'));
+  });
+});
